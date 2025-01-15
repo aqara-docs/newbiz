827517fab9b03d05de8a51331eff5ffea97d90d2
aqara-docs <155663374+aqara-docs@users.noreply.github.com>
2025-01-15 15:21:27 +0900

    20250115-3

--- pages/08_LocalGPT_OLLAMA_FILE.py
+++ pages/08_LocalGPT_OLLAMA_FILE.py
@@ -38,7 +38,7 @@ class ChatCallbackHandler(BaseCallbackHandler):
 
 
 llm = ChatOllama(
-    model="EEVE-Korean-10.8B:latest",
+    model="gemma2",
     temperature=0.1,
     streaming=True,
     callbacks=[
@@ -73,7 +73,7 @@ def embed_file(file):
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
     
-    embeddings = OllamaEmbeddings(model="EEVE-Korean-10.8B:latest")
+    embeddings = OllamaEmbeddings(model="gemma2")
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
--- pages/08_LocalGPT_OLLAMA_MULTIPLE_MD.py
+++ pages/08_LocalGPT_OLLAMA_MULTIPLE_MD.py
@@ -38,7 +38,7 @@ class ChatCallbackHandler(BaseCallbackHandler):
 
 
 llm = ChatOllama(
-    model="EEVE-Korean-10.8B:latest",
+    model="gemma2",
     temperature=0.1,
     streaming=True,
     callbacks=[
@@ -75,7 +75,7 @@ def embed_files(files):
         docs = loader.load_and_split(text_splitter=splitter)
         all_docs.extend(docs)
     
-    embeddings = OllamaEmbeddings(model="EEVE-Korean-10.8B:latest")
+    embeddings = OllamaEmbeddings(model="gemma2")
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     
     vectorstore = FAISS.from_documents(all_docs, cached_embeddings)
